54bdb267bd33f8adf2d2e1a413022947f85653c9
aziz158 <mi6xino@gmail.com>
2026-01-20 13:43:05 -0800

    leetcode 300 solution submission

---
+++ 300_medium_longest_inceasing_subsequence.py
@@ -0,0 +1,52 @@
+import unittest
+from typing import *
+
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+ 
+
+# Example 1:
+
+# Input: nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+# Example 2:
+
+# Input: nums = [0,1,0,3,2,3]
+# Output: 4
+# Example 3:
+
+# Input: nums = [7,7,7,7,7,7,7]
+# Output: 1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 2500
+# -104 <= nums[i] <= 104
+
+def lengthOfLIS(nums: List[int]) -> int:
+
+    dp = [1] * len(nums)
+
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+
+    return max(dp)
+
+class TestRobber(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(lengthOfLIS([10,9,2,5,3,7,101,18]), 4)
+
+    def test2(self):
+        self.assertEqual(lengthOfLIS([0,1,0,3,2,3]), 4)
+
+    def test3(self):
+        self.assertEqual(lengthOfLIS([7,7,7,7,7,7,7]), 1)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
